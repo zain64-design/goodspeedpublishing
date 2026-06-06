8ddebf670d90cd647f55fc86286917c66f4c4438
zain64-design <zainyzain64@example.com>
2026-06-06 06:49:12 +0500

    new changes

--- src/app/(webRoutes)/page.tsx
+++ src/app/(webRoutes)/page.tsx
@@ -4,7 +4,7 @@ import Story from "@/app/_components/webComp/story/Story";
 import FAQ from "@/app/_components/webComp/faq/FAQ";
 import Banner from "@/app/_components/webComp/banner/Banner";
 import Announcement from "@/app/_components/webComp/banner/Announcement";
-import Stats from "../_components/webComp/stats/Stats";
+import Stats from "@/app/_components/webComp/stats/Stats";
 
 export default function Home() {
     return (
