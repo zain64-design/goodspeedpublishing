b45712e313b456debb6e96967a5e4c4ae0762dc2
zain64-design <zainyzain64@gmail.com>
2026-06-09 00:56:38 +0500

    banner right box image aspect ratio changed

--- src/app/_components/webComp/banner/BoxRight.tsx
+++ src/app/_components/webComp/banner/BoxRight.tsx
@@ -22,8 +22,8 @@ export default function BoxRight({}: Props) {
     desktopSrc={RightBoxThumb}
     mobileSrc={RightBoxThumbMobile}
     alt="hero"
-    desktopWidth={409}
-    desktopHeight={480}
+    desktopWidth={612}
+    desktopHeight={719}
     mobileWidth={342}
     mobileHeight={419}
     fetchPriority="high"
