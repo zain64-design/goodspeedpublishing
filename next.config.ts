0fa9683650d07fd45d5944c77cb9fc40504d256f
zain64-design <zainyzain64@gmail.com>
2026-06-11 03:54:12 +0500

    tweaks on config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-        qualities: [100, 25, 50, 70, 75],
+        qualities: [100, 25, 50, 70, 75, 80],
     }
 };
 
