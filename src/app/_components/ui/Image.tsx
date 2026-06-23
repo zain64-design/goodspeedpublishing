e7bc172124c1044906e5bb2a742c59b91fd1e417
zain64-design <zainyzain64@gmail.com>
2026-06-23 05:46:04 +0500

    image comp changes

--- src/app/_components/ui/Image.tsx
+++ src/app/_components/ui/Image.tsx
@@ -3,6 +3,6 @@ import type { ImageProps } from "@/app/_types"
 
 export default function ImageComp({ src, width, height, quality, sizes, alt, className, style, fill, placeholder, blurDataURL, loading = "lazy", fetchPriority, preload, unoptimized = false, }: ImageProps) {
     return (
-        <Image src={src} width={width} height={height} quality={quality} sizes={sizes} alt={alt} className={className} style={style} fill={fill} placeholder={placeholder} blurDataURL={blurDataURL} loading={preload ? "eager" : loading}  fetchPriority={preload ? "high" : fetchPriority} preload={preload} unoptimized={unoptimized}   />
+        <Image src={src} width={width} height={height} quality={quality} sizes={sizes} alt={alt} className={className} style={style} fill={fill} placeholder={placeholder} blurDataURL={blurDataURL} loading={preload ? undefined : loading}   fetchPriority={preload ? undefined : fetchPriority} preload={preload} unoptimized={unoptimized}   />
     )
 }
