95cb0b11cac3c02379d6690519e081b1b4884f2e
zain64-design <zainyzain64@gmail.com>
2026-06-24 02:43:46 +0500

    new changes in banner image sizes

--- src/app/_components/webComp/banner/BoxRight.tsx
+++ src/app/_components/webComp/banner/BoxRight.tsx
@@ -18,12 +18,12 @@ export default function BoxRight({}: Props) {
       }}>
 <ImageComp
     src={RightBoxThumb}
-    width={409}
-    height={480}
-    sizes="(max-width: 575px) 100vw, (max-width: 767px) 50vw, 409px"
+    width={613}
+    height={903}
+    sizes="(max-width: 575px) 60vw, (max-width: 767px) 45vw, 326px"
     alt="hero"
     fetchPriority="high"
-    className="max-w-full max-h-80 sm:max-h-85 md:max-h-100 lg:max-h-100 xl:max-h-120 2xl:max-h-160 4xl:max-h-180 mx-auto block object-contain xl:scale-[1.05] 4xl:scale-[1.2]"
+    className="max-w-fit sm:max-w-full max-h-80 sm:max-h-85 md:max-h-100 lg:max-h-100 xl:max-h-120 2xl:max-h-160 4xl:max-h-180 mx-auto block object-contain xl:scale-[1.05]"
 />
     </div>
   )
