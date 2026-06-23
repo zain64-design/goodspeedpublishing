1102366a86e0af91271815ae425d47e8dafe3d99
zain64-design <zainyzain64@gmail.com>
2026-06-24 02:51:57 +0500

    new change lcp for mobile

--- src/app/_components/webComp/banner/BoxRight.tsx
+++ src/app/_components/webComp/banner/BoxRight.tsx
@@ -20,9 +20,10 @@ export default function BoxRight({}: Props) {
     src={RightBoxThumb}
     width={613}
     height={903}
-    sizes="(max-width: 575px) 60vw, (max-width: 767px) 45vw, 326px"
+    sizes="(max-width: 575px) 80vw, (max-width: 767px) 45vw, 326px"
     alt="hero"
     fetchPriority="high"
+    loading="eager"
     className="max-w-fit sm:max-w-full max-h-80 sm:max-h-85 md:max-h-100 lg:max-h-100 xl:max-h-120 2xl:max-h-160 4xl:max-h-180 mx-auto block object-contain xl:scale-[1.05]"
 />
     </div>
