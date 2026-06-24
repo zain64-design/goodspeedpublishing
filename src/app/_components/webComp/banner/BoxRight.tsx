ffb257aa8c9f6148555099b2aaf31c7cbf1fdb02
zain64-design <zainyzain64@gmail.com>
2026-06-24 08:51:41 +0500

    final img changes

--- src/app/_components/webComp/banner/BoxRight.tsx
+++ src/app/_components/webComp/banner/BoxRight.tsx
@@ -20,7 +20,7 @@ export default function BoxRight({}: Props) {
     src={RightBoxThumb}
     width={613}
     height={903}
-    sizes="(max-width: 575px) 320px, (max-width: 767px) 45vw, 326px"
+    sizes="(max-width: 575px) 380px, (max-width: 767px) 45vw, 326px"
     alt="hero"
     preload
     className="max-w-auto sm:max-w-full max-h-80 sm:max-h-85 md:max-h-100 lg:max-h-100 xl:max-h-120 2xl:max-h-160 4xl:max-h-180 mx-auto block object-contain xl:scale-[1.05]"
