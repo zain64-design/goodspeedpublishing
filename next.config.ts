4e576b542a8874a8d935ada5d3199038c77785d7
zain64-design <zainyzain64@gmail.com>
2026-06-26 10:44:10 +0500

    next config changes for image

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ const nextConfig: NextConfig = {
     inlineCss: true,
   },
   images: {
+deviceSizes: [384, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384, 420, 480],
         qualities: [25, 50, 60, 65, 70, 75, 80, 85, 90, 95, 100],
     }
