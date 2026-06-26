b25dfc6740c19e3914430276317daa1f7688f177
zain64-design <zainyzain64@gmail.com>
2026-06-26 16:34:13 +0500

    new opt

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,7 @@ const nextConfig: NextConfig = {
   images: {
 deviceSizes: [280, 576, 768, 992, 1200, 1400, 1600, 1800, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-        qualities: [25, 50, 75, 100],
+        qualities: [25, 50, 65, 75, 100],
     }
 };
 
