e6e11dfddcdff60c1a1c76448141410aa74b03aa
zain64-design <zainyzain64@gmail.com>
2026-06-26 10:38:05 +0500

    new change in story image and next config

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ const nextConfig: NextConfig = {
     inlineCss: true,
   },
   images: {
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384, 420, 480],
         qualities: [25, 50, 60, 65, 70, 75, 80, 85, 90, 95, 100],
     }
 };
