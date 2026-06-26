f20407f665db4b2662ea6c133b6a88efbc15ecc7
zain64-design <zainyzain64@gmail.com>
2026-06-26 05:53:09 +0500

    comment code removed

--- src/app/_components/ui/SmoothLayout.tsx
+++ src/app/_components/ui/SmoothLayout.tsx
@@ -1,36 +1,3 @@
-// 'use client'
-// import { useRef } from 'react'
-// import { ScrollSmoother, useGSAP, ScrollTrigger } from '@/app/_libs/gsap'
-
-// export default function SmoothLayout({ children }: { children: React.ReactNode }) {
-//     const wrapperRef = useRef<HTMLDivElement>(null)
-//     const contentRef = useRef<HTMLDivElement>(null)
-
-//     useGSAP(() => {
-//         if (!wrapperRef.current || !contentRef.current) return
-
-//         const smoother = ScrollSmoother.create({
-//             wrapper: wrapperRef.current,
-//             content: contentRef.current,
-//             smooth: 1,
-//             effects: true,  
-//         })
-
-//         return () => {
-//             smoother.kill()
-//             ScrollTrigger.getAll().forEach(t => t.kill())
-//         }
-//     }, { scope: wrapperRef })
-
-//     return (
-//         <div ref={wrapperRef}>
-//             <div ref={contentRef}>
-//                 {children}
-//             </div>
-//         </div>
-//     )
-// }
-
 'use client'
 import { useRef } from 'react'
 import { ScrollSmoother, useGSAP, ScrollTrigger } from '@/app/_libs/gsap'
