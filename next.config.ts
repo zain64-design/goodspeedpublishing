6730a1f1413612ccc8ccefcff090be0eee7ff15a
zain64-design <zainyzain64@gmail.com>
2026-06-27 03:16:19 +0500

    add browser list

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
      experimental: {
     inlineCss: true,
-    
   },
   images: {
 deviceSizes: [280, 576, 768, 992, 1200, 1400, 1600, 1800, 2048, 3840],
