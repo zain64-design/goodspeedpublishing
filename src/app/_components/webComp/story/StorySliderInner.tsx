fdb587e86c8a5dbfb40981490448e79697151978
zain64-design <zainyzain64@gmail.com>
2026-06-26 13:32:24 +0500

    new image changes

--- src/app/_components/webComp/story/StorySliderInner.tsx
+++ src/app/_components/webComp/story/StorySliderInner.tsx
@@ -42,9 +42,10 @@ export default function StorySliderInner({story}: Props) {
             {data?.map(({ id, title, img }) => (
               <SwiperSlide key={id}>
                 <div className="items-center md:pt-15 lg:pt-20 xl:pt-23.25">
-                  <div className="text-center p-4">
+                  <div className="text-center">
                     <Text as="p" className="text-black-text-100 capitalize font-playfair italic font-bold text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl 4xl:text-5xl mb-2 md:mb-6">{title}</Text>
-                    <ImageComp src={img} width={477} height={311} sizes="(max-width: 767px) 100vw, 420px" alt="story book" className="w-full max-h-166.25 h-auto mx-auto object-contain object-center drop-shadow-[-5px_3px_9px_rgba(0,0,0,0.80)]" />
+                    {/* <ImageComp src={img} width={477} height={311} sizes="(max-width: 767px) 100vw, 420px" alt="story book" className="w-full max-h-166.25 h-auto mx-auto object-contain object-center drop-shadow-[-5px_3px_9px_rgba(0,0,0,0.80)]" /> */}
+                                        <ImageComp src={img} width={640} height={414} sizes="(max-width: 767px) 100vw, 640px" alt="story book" className="w-full max-h-[414px] mx-auto object-contain object-center" />
                   </div>
                 </div>
               </SwiperSlide>
