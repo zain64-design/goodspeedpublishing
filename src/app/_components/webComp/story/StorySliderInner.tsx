daef2b401f439d7568eab540f8960582286ca918
zain64-design <zainyzain64@gmail.com>
2026-06-26 15:07:07 +0500

    remove w-full from story img added eager on portfolio slider

--- src/app/_components/webComp/story/StorySliderInner.tsx
+++ src/app/_components/webComp/story/StorySliderInner.tsx
@@ -45,7 +45,7 @@ export default function StorySliderInner({story}: Props) {
                   <div className="text-center">
                     <Text as="p" className="text-black-text-100 capitalize font-playfair italic font-bold text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl 4xl:text-5xl mb-2 md:mb-6">{title}</Text>
                     {/* <ImageComp src={img} width={477} height={311} sizes="(max-width: 767px) 100vw, 420px" alt="story book" className="w-full max-h-166.25 h-auto mx-auto object-contain object-center drop-shadow-[-5px_3px_9px_rgba(0,0,0,0.80)]" /> */}
-                                        <ImageComp src={img} width={665} height={430} sizes="(max-width: 767px) 100vw, 665px" alt="story book" className="w-full max-h-[430px] mx-auto object-contain object-center" />
+                                        <ImageComp src={img} width={665} height={430} sizes="(max-width: 767px) 100vw, 665px" alt="story book" className="w-auto max-h-[430px] max-w-full mx-auto" />
                   </div>
                 </div>
               </SwiperSlide>
