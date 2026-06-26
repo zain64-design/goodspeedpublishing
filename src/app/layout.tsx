2560e1a7eb2ed43d6d13454ea3e7800f45a44c0f
zain64-design <zainyzain64@gmail.com>
2026-06-27 02:57:59 +0500

    tag manager removed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,34 +22,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
   return (
     <html lang="en" className={`${poppins.variable} ${playfair.variable} h-full antialiased`}>
       <body className="">
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-K52MGDH2"
-            height="0"
-            width="0"
-            style={{ display: 'none', visibility: 'hidden' }}
-          />
-        </noscript>
         {children}
-        <Script
-          id="gtm-script"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `!function(e,t,a,r,g){e[r]=e[r]||[],e[r].push({"gtm.start":new Date().getTime(),event:"gtm.js"});var n=t.getElementsByTagName(a)[0],s=t.createElement(a);s.async=!0,s.src="https://www.googletagmanager.com/gtm.js?id="+g+("dataLayer"!=r?"&l="+r:""),n.parentNode.insertBefore(s,n)}(window,document,"script","dataLayer","GTM-K52MGDH2");`,
-          }}
-        />
-        <Script
-          id="gtag-js"
-          strategy="afterInteractive"
-          src="https://www.googletagmanager.com/gtag/js?id=AW-11316340682"
-        />
-        <Script
-          id="gtag-config"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `function gtag(){dataLayer.push(arguments)}window.dataLayer=window.dataLayer||[],gtag("js",new Date),gtag("config","AW-11316340682");`,
-          }}
-        />
       </body>
     </html>
   );
