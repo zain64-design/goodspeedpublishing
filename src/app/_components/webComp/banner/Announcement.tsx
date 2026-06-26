178234befc0af22da8044fff433135b644a80deb
zain64-design <zainyzain64@gmail.com>
2026-06-26 17:46:37 +0500

    remove animate from announcement

--- src/app/_components/webComp/banner/Announcement.tsx
+++ src/app/_components/webComp/banner/Announcement.tsx
@@ -6,7 +6,7 @@ export default function Announcement() {
     <>
     <section className='py-4 xl:pt-9 xl:pb-12.5'>
         <div className='md:container mx-auto px-4'>
-            <Text as="h2" animate className='text-mid text-center text-[20px] leading-9 sm:text-[28px] sm:leading-12 md:text-[30px] md:leading-14 lg:text-[38px] lg:leading-16 xl:text-[48px] xl:leading-18 font-bold tracking-[-1.44px]'>Unlock Your <Badge label="35% Discount" variant="secondary" className=''/> Claim Your Plan Now Now and share your story <Badge label="beautifully and boldly" variant="primary"/></Text>
+            <Text as="h2" className='text-mid text-center text-[20px] leading-9 sm:text-[28px] sm:leading-12 md:text-[30px] md:leading-14 lg:text-[38px] lg:leading-16 xl:text-[48px] xl:leading-18 font-bold tracking-[-1.44px]'>Unlock Your <Badge label="35% Discount" variant="secondary" className=''/> Claim Your Plan Now Now and share your story <Badge label="beautifully and boldly" variant="primary"/></Text>
         </div>
     </section>
     <hr className='border-t border-mid  md:container mx-auto px-4 my-6 md:my-8 xl:my-12.5' />
