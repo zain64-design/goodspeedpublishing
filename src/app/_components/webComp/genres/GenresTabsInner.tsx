a5a381f0dcdf3fd9df8a849b7cf3fc9d647f5648
zain64-design <zainyzain64@gmail.com>
2026-06-30 00:46:26 +0500

    extra images removed

--- src/app/_components/webComp/genres/GenresTabsInner.tsx
+++ src/app/_components/webComp/genres/GenresTabsInner.tsx
@@ -31,7 +31,7 @@ export default function GenresTabsInner({ genres }: Props) {
                                     </div>
                                 </div>
                                 {thumbnail && (
-                                    <ImageComp src={thumbnail} width={670} height={435} sizes="(max-width: 575px) 352px, (max-width: 991px) 50vw, 670px" className='object-contain max-h-108.75 mx-auto w-auto reflect-below' alt={`image-${category}`} />
+                                    <ImageComp src={thumbnail} width={670} height={435} sizes="(max-width: 575px) 352px, (max-width: 991px) 50vw, 670px" className='object-contain h-auto mx-auto max-w-167.5 w-full reflect-below' alt={`image-${category}`} />
                                 )}
                             </div>
                         </Activity>
