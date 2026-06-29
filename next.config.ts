3d672c3cf9458663ee26c1dfcb8add8e34dd366e
zain64-design <zainyzain64@gmail.com>
2026-06-30 04:19:58 +0500

    new changes package json

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
      experimental: {
     inlineCss: true,
   },
+    transpilePackages: ['@intl-tel-input/react', 'intl-tel-input'],
   images: {
     deviceSizes: [280, 412, 576, 768, 992, 1200, 1400, 1600, 1800, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 220, 256, 380, 440, 480, 540, 507, 512, 560, 580, 616, 640, 680, 700, 720, 740, 780, 840, 880],
