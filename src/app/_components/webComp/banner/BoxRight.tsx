f0593838d09f42a29282f7e6c9a6b175d9879ab9
zain64-design <zainyzain64@gmail.com>
2026-07-01 05:21:45 +0500

    banner image size reverted again

--- src/app/_components/webComp/banner/BoxRight.tsx
+++ src/app/_components/webComp/banner/BoxRight.tsx
@@ -21,7 +21,7 @@ export default function BoxRight({}: Props) {
     width={1222}
     height={1803}
     quality={65}
-    sizes="(max-width: 575px) 124px, (max-width: 767px) 45vw, 500px"
+    sizes="(max-width: 575px) 124px, (max-width: 767px) 45vw, 326px"
     alt="hero"
     fetchPriority="high"
     loading="eager"
