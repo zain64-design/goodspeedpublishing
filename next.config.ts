ff60eb951302198c2e5709b917180e73f22f58a8
zain64-design <zainyzain64@gmail.com>
2026-07-02 04:50:43 +0500

    new tweaks

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-     experimental: {
+  experimental: {
     inlineCss: true,
-        optimizePackageImports: [
-      'react-icons',
-      'gsap',
-      'primereact',
-      'swiper',
-    ],
+    optimizePackageImports: ["react-icons", "gsap", "primereact", "swiper"],
   },
-    transpilePackages: ['@intl-tel-input/react', 'intl-tel-input'],
+  transpilePackages: ["@intl-tel-input/react", "intl-tel-input"],
   images: {
     deviceSizes: [280, 412, 576, 768, 992, 1200, 1400, 1600, 1800, 2048, 3840],
-    imageSizes: [16, 32, 48, 64, 96, 128, 220, 256, 380, 440, 480, 540, 507, 512, 560, 580, 616, 640, 680, 700, 720, 740, 780, 840, 880],
+    imageSizes: [
+      16, 32, 48, 64, 96, 128, 220, 256, 380, 440, 480, 540, 507, 512, 560, 580,
+      616, 640, 680, 700, 720, 740, 780, 840, 880,
+    ],
 
-        qualities: [25, 50, 65, 75, 100],
-    }
+    qualities: [25, 50, 65, 75, 100],
+  },
 };
 
-export default nextConfig
+export default nextConfig;
