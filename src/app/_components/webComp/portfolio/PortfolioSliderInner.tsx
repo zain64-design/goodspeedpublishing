c138934553891e70b70f6e10c3201fcba6242fbc
zain64-design <zainyzain64@gmail.com>
2026-07-01 22:08:07 +0500

    placeholder added on slider images

--- src/app/_components/webComp/portfolio/PortfolioSliderInner.tsx
+++ src/app/_components/webComp/portfolio/PortfolioSliderInner.tsx
@@ -19,7 +19,8 @@ export default function PortfolioSliderInner({portfolio}: Props) {
                 spaceBetween={40}
                 grabCursor={true}
                 loop={true}
-                autoplay={{ delay: 1500, disableOnInteraction: false, pauseOnMouseEnter: true }}
+                // autoplay={{ delay: 1500, disableOnInteraction: false, pauseOnMouseEnter: true }}
+                autoplay={false}
                 speed={500}
                 slidesPerView={5}
                 centeredSlides={true}
@@ -57,7 +58,7 @@ export default function PortfolioSliderInner({portfolio}: Props) {
                         <div className="transition-transform duration-500 ease-in-out item-main scale-[.7] pt-12.5 sm:pt-22">
                             <div className={twMerge(`bg-ct shadow-[inset_-7px_-1.5px_14.1px_0_rgba(0,0,0,0.15)] rounded-[15px] sm:rounded-[30px] md:rounded-[40px] xl:rounded-[46px] 2xl:rounded-[50px] 4xl:rounded-[58px] 6xl:rounded-[63px] mb-0 sm:mb-7.5 xl:mb-10`)}>
                             <div className="overlay-shadow relative rotate-20">
-                                <ImageComp src={img} width={370} height={585} loading='eager' sizes="(max-width: 991px) 33vw, 250px" alt="portfolio book" className="max-w-auto max-h-110 lg:max-h-146.25 mx-auto object-contain object-center drop-shadow-[5px_8px_5px_rgba(0,0,0,0.30)]" />
+                                <ImageComp src={img} width={370} height={585} placeholder="blur" sizes="(max-width: 991px) 33vw, 250px" alt="portfolio book" className="max-w-auto max-h-110 lg:max-h-146.25 mx-auto object-contain object-center drop-shadow-[5px_8px_5px_rgba(0,0,0,0.30)]" />
                             </div>
                         </div>
                         <Text as="span" className="inline-flex text-black-text-100 capitalize justify-center items-center font-semibold p-2 content-center text-xs sm:text-base md:text-xl 2xl:text-2xl rounded-full w-full min-h-10 max-w-40 sm:min-h-12 sm:max-w-55 md:max-w-66.75 md:min-h-18.25 bg-primary-200">{title}</Text>
